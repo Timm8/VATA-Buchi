48637cd842695a32ee7842e6cd96627f4a79de25
Tom <tomas.lascak@gmail.com>
2014-06-01 00:59:33 +0200

    push

--- libvata/build/cli/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/BP/vata/libvata/cli
-# Build directory: /home/tom/BP/vata/libvata/build/cli
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
